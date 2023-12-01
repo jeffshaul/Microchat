0756df7b954ecb91bc17dff0f6abfef985e7fd6c
Jeff <jeff@gosign.ai>
2023-12-01 01:08:00 -0500

    Initial Commit

---
+++ Microchat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Microchat
+//
+//  Created by Jeff Shaul on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Microchat/MicrochatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicrochatApp.swift
+//  Microchat
+//
+//  Created by Jeff Shaul on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MicrochatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
